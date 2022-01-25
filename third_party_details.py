911a0a916d1099c91cb2b60bc9e0b8e854d8924a
Gabriel Rafailov <grafailov@fortinet.com>
2022-01-25 21:35:44 +0200

    attach link to allure report in jira description

--- third_party_details.py
+++ third_party_details.py
@@ -4,5 +4,4 @@ USER_NAME = 'automation'
 PASSWORD = 'Aut0g00dqa42'
 SHARED_DRIVE_PATH = r'\\ens-fs01.ensilo.local'
 SHARED_DRIVE_VERSIONS_PATH = fr'{SHARED_DRIVE_PATH}\Versions'
-JENKINS_JOB = os.getenv("BUILD_URL") if os.getenv("BUILD_URL") is not None else None
-
+JENKINS_JOB = fr'{os.getenv("BUILD_URL")}/allure/' if os.getenv("BUILD_URL") is not None else None
