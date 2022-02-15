eae3001166fa0bfefe9388c25587becbb6eacae8
Gabriel Rafailov <grafailov@fortinet.com>
2022-02-15 13:12:01 -0800

    Hotfix/kill malware processes before running it

--- infra/system_components/windows_collector.py
+++ infra/system_components/windows_collector.py
@@ -531,7 +531,8 @@ msiexec.exe /x %val% /qn UPWD="{registration_password}" RMCONFIG=1 /l*vx {uninst
             files_to_copy=[malware_name])
 
         pids = self.os_station.get_service_process_ids(service_name=malware_name)
-        for single_pid in pids:
-            self.os_station.kill_process_by_id(pid=single_pid)
+        if pids is not None:
+            for single_pid in pids:
+                self.os_station.kill_process_by_id(pid=single_pid)
 
         self.os_station.execute_cmd(f'{target_folder}\\{malware_name}', asynchronous=True)
