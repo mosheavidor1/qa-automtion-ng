4eb6a9d62ec77918a8b362ef38c418836d52730d
Yosef Cohen <coheny@fortinet.com>
2022-02-21 07:36:57 -0800

    Yosef testim new

--- tests/audit_tests/tests_audit.py
+++ tests/audit_tests/tests_audit.py
@@ -5,14 +5,17 @@ from tests.audit_tests.audit_tests_base import AuditTestsBase
 
 @allure.epic("Management")
 @allure.feature("Audit")
-# @pytest.mark.sanity
+@pytest.mark.sanity
 class AuditTests(AuditTestsBase):
 
     @pytest.mark.xray('EN-73328')
-    # @pytest.mark.testim_sanity
-    def test_audit(self, management):
+    def test_verify_audit_file_was_downloaded(self, management):
         """
-        This test run Testim.io to create Audit report
+        1. Login
+        2. go to Administrator > Tools page
+        3. In the Audit trail, insert start data from 30 days ago and until to today
+        4. click on generate audit button
+        5. Verify that file was downloaded
         """
         self.management = management
         self.collector = self.management.collectors[0]
