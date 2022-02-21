9f02a6a49403919480fcd5b47ab2ab5999b0b48c
oturgeman <oturgeman@fortinet.com>
2022-02-21 18:23:05 +0200

    add comments

--- tests/exceptions_tests/tests_exception_sanity.py
+++ tests/exceptions_tests/tests_exception_sanity.py
@@ -13,7 +13,31 @@ class ExceptionsTests(ExceptionsTestsBase):
     # @pytest.mark.testim_sanity
     # Full_covered_exception - event excepted
     def test_create_full_covered_exception(self, management):
+        """
+        steps:
+        1. create event DynamicCodeTests
+        2. crete exception for DynamicCodeTests
+        3. create same event - event should not be created
+        """
         self.test_type = ExceptionTestType.CREATE_FULL_COVERED_EXCEPTION
+        self.management = management
+        self.collector = self.management.collectors[0]
+        self.malware_name = "DynamicCodeTests.exe"
+        self.play_test()
+
+    @pytest.mark.xray('EN-68890')
+    @pytest.mark.testim_sanity
+    # Partially covered exception - event excepted
+    def test_create_partially_covered_exception(self, management):
+        """
+        steps:
+        1. create event DynamicCodeTests
+        2. crete exception for DynamicCodeTests with empty group
+        3. move collector to the empty group
+        4. assign group to policies
+        5. create same event - event should not be created
+        """
+        self.test_type = ExceptionTestType.CREATE_PARTIALLY_COVERED_EXCEPTION
         self.management = management
         self.collector = self.management.collectors[0]
         self.malware_name = "DynamicCodeTests.exe"
@@ -23,6 +47,12 @@ class ExceptionsTests(ExceptionsTestsBase):
     # @pytest.mark.testim_sanity
     # Partially covered exception - event created
     def test_create_partially_covered_exception_event_created(self, management):
+        """
+        steps:
+        1. create event DynamicCodeTests
+        2. crete exception for DynamicCodeTests with empty group
+        3. create same event - event should be created
+        """
         self.test_type = ExceptionTestType.CREATE_PARTIALLY_COVERED_EXCEPTION_EVENT_CREATED
         self.management = management
         self.collector = self.management.collectors[0]
@@ -32,6 +62,12 @@ class ExceptionsTests(ExceptionsTestsBase):
     @pytest.mark.xray('EN-68885')
     # @pytest.mark.testim_sanity
     def test_edit_fully_covered_exception(self, management):
+        """
+        steps:
+        1. execute test_create_full_covered_exception
+        2. edit exception - change group to empty group
+        3. edit exception - change destination to specific destination
+        """
         self.test_type = ExceptionTestType.EDIT_FULL_COVERED_EXCEPTION
         self.management = management
         self.collector = self.management.collectors[0]
@@ -41,17 +77,12 @@ class ExceptionsTests(ExceptionsTestsBase):
     @pytest.mark.xray('EN-68888')
     # @pytest.mark.testim_sanity
     def test_edit_partially_covered_exception(self, management):
+        """
+        steps:
+        1. execute test_create_partially_covered_exception
+        2. edit exception - change destination to specific destination
+        """
         self.test_type = ExceptionTestType.EDIT_PARTIALLY_COVERED_EXCEPTION
-        self.management = management
-        self.collector = self.management.collectors[0]
-        self.malware_name = "DynamicCodeTests.exe"
-        self.play_test()
-
-    @pytest.mark.xray('EN-68890')
-    # @pytest.mark.testim_sanity
-    # Partially covered exception - event excepted
-    def test_create_partially_covered_exception(self, management):
-        self.test_type = ExceptionTestType.CREATE_PARTIALLY_COVERED_EXCEPTION
         self.management = management
         self.collector = self.management.collectors[0]
         self.malware_name = "DynamicCodeTests.exe"
