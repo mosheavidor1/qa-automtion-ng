f2a6085d3bb43c6454385e09da7c4c2b2df7b742
Leonid Fredlin <lfredlin@fortinet.com>
2022-06-29 05:56:34 -0700

    AUT-1241: Wait until collector will get the configuration

--- tests/utils/collector_utils.py
+++ tests/utils/collector_utils.py
@@ -1,10 +1,13 @@
 import allure
 import logging
-
+from infra.system_components.collector import CollectorAgent
+from infra.multi_tenancy.tenant import Tenant
+from infra import common_utils
 logger = logging.getLogger(__name__)
 
 KEEPALIVE_INTERVAL = 5
 STATUS_TIMEOUT = 5 * 60
+MAX_WAIT_FOR_CONFIGURATION = 5 * 60  # Arbitrary value
 
 
 class CollectorUtils:
@@ -18,3 +21,33 @@ class CollectorUtils:
         management.tenant.rest_api_client.policies.assign_policy('Exfiltration Prevention', group_name, timeout=1)
         management.tenant.rest_api_client.policies.assign_policy('Execution Prevention', group_name, timeout=1)
         management.tenant.rest_api_client.policies.assign_policy('Ransomware Prevention', group_name)
+
+    @staticmethod
+    @allure.step("Wait for configuration")
+    def wait_for_configuration(collector_agent: CollectorAgent, tenant: Tenant, start_collector=True,
+                               timeout=None, interval_sec=None):
+        """ Wait until collector agent get updated configuration: the indication is a successful stop action
+        with the updated registration password """
+        timeout = timeout or MAX_WAIT_FOR_CONFIGURATION
+        interval_sec = interval_sec or KEEPALIVE_INTERVAL
+        logger.info(f"Wait until {collector_agent} will get the new configuration")
+        rest_collector = tenant.rest_components.collectors.get_by_ip(ip=collector_agent.host_ip)
+        registration_password = tenant.organization.registration_password
+
+        def condition():
+            try:
+                logger.info(f"Try to stop collector with the updated registration password {registration_password}")
+                collector_agent.stop_collector(password=registration_password)
+                return True
+            except Exception as e:
+                logger.info(f"Failed to stop collector, try again. Got: {e}")
+                return False
+
+        common_utils.wait_for_condition(condition_func=condition, timeout_sec=timeout, interval_sec=interval_sec)
+        collector_agent.wait_until_agent_down()
+        if start_collector:
+            collector_agent.start_collector()
+            collector_agent.wait_until_agent_running()
+            rest_collector.wait_until_running()
+
+
--- tests/utils/tenant_utils.py
+++ tests/utils/tenant_utils.py
@@ -5,20 +5,28 @@ from infra.system_components.management import Management
 from infra.api.management_api.collector import RestCollector
 from infra.utils.utils import StringUtils
 from infra.multi_tenancy.tenant import Tenant
+from infra.system_components.collector import CollectorAgent
+from tests.utils.collector_utils import CollectorUtils
 
 logger = logging.getLogger(__name__)
 
 
 @contextmanager
-def new_tenant_context(management: Management, rest_collector: RestCollector, user_name=None, user_password=None,
+def new_tenant_context(management: Management, collector_agent: CollectorAgent, user_name=None, user_password=None,
                        org_name=None, reg_password=None) -> ContextManager[Tuple[Tenant, RestCollector]]:
+    """ Context for creating new tenant with collector, at the end return the collector back to the main tenant
+    and delete the new tenant.
+    After returning collector back to main tenant and before deleting the new tenant, must to wait until collector
+    received back the configuration of the main tenant, otherwise it will search for a deleted tenant endless loop.
+    """
     user_name = user_name or generate_username()
     user_password = user_password or generate_user_password()
     org_name = org_name or generate_org_name()
     reg_password = reg_password or generate_registration_password()
-    logger.info(f"Create new temp tenant with user {user_name}, organization {org_name}, {rest_collector}")
 
     main_tenant = management.tenant
+    rest_collector = main_tenant.rest_components.collectors.get_by_ip(ip=collector_agent.host_ip)
+    logger.info(f"Create new temp tenant with user {user_name}, organization {org_name}, {rest_collector}")
     new_tenant = management.create_temp_tenant(user_name=user_name, user_password=user_password,
                                                organization_name=org_name, registration_password=reg_password)
     logger.info(f"Move {rest_collector} from main {main_tenant} to target {new_tenant}")
@@ -28,6 +36,9 @@ def new_tenant_context(management: Management, rest_collector: RestCollector, us
     finally:
         logger.info(f"Return back the {target_rest_collector} to the main tenant {main_tenant}")
         main_tenant.require_ownership_over_collector(source_collector=target_rest_collector)
+        logger.info(f"Wait until {collector_agent} will get the configuration from {main_tenant}")
+        CollectorUtils.wait_for_configuration(collector_agent=collector_agent, tenant=main_tenant,
+                                              start_collector=True)
         logger.info(f"Validate that temp tenant {new_tenant} has no collectors and delete it")
         remaining_rest_collectors = new_tenant.rest_components.collectors.get_all(safe=True)
         assert remaining_rest_collectors is None, f"Temp {new_tenant} still has collectors: {remaining_rest_collectors}"
