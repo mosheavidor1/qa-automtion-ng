19b5a088da9368687933f3a7a68fc2a588c30583
Ido Schwartz <ischwartz@fortinet.com>
2022-08-23 12:35:53 +0300

    [AUT-1320] refactor part of the code for better readability

--- environment_creation_tests/desired_env_details.py
+++ environment_creation_tests/desired_env_details.py
@@ -1,30 +1,34 @@
 import os
 
-environment_name = os.getenv("environment_name") if os.getenv("environment_name") is not None else None
-curr_version = '5.2.0.x'
+environment_name = os.getenv("environment_name")
+curr_version = "5.2.0.x"
 
-management_version = os.getenv("management_version") if os.getenv("management_version") is not None else curr_version
-aggregator_version = os.getenv("aggregator_version") if os.getenv("aggregator_version") is not None else curr_version
-core_version = os.getenv("core_version") if os.getenv("core_version") is not None else curr_version
-windows_collector_version = os.getenv("windows_collector_version") if os.getenv("windows_collector_version") is not None else curr_version
-linux_collector_version = os.getenv("linux_collector_version") if os.getenv("linux_collector_version") is not None else curr_version
+management_version = os.getenv("management_version", default=curr_version)
+aggregator_version = os.getenv("aggregator_version", default=curr_version)
+core_version = os.getenv("core_version", default=curr_version)
+windows_collector_version = os.getenv("windows_collector_version", default=curr_version)
+linux_collector_version = os.getenv("linux_collector_version", default=curr_version)
 
-management_and_aggregator_deployment_architecture = os.getenv("management_and_aggregator_deployment_architecture") if os.getenv("management_and_aggregator_deployment_architecture") is not None else 'both'
+management_and_aggregator_deployment_architecture = os.getenv(
+    "management_and_aggregator_deployment_architecture", default="both"
+)
 
-aggregators_amount = int(os.getenv("aggregators_amount")) if os.getenv("aggregators_amount") is not None else 1
-cores_amount = int(os.getenv("cores_amount")) if os.getenv("cores_amount") is not None else 1
-windows_11_64_bit = int(os.getenv("windows_11_64_bit")) if os.getenv("windows_11_64_bit") is not None else 0
-windows_10_64_bit = int(os.getenv("windows_10_64_bit")) if os.getenv("windows_10_64_bit") is not None else 0
-windows_10_32_bit = int(os.getenv("windows_10_32_bit")) if os.getenv("windows_10_32_bit") is not None else 0
-windows_8_64_bit = int(os.getenv("windows_8_64_bit")) if os.getenv("windows_8_64_bit") is not None else 0
-windows_8_32_bit = int(os.getenv("windows_8_32_bit")) if os.getenv("windows_8_32_bit") is not None else 0
-windows_7_64_bit = int(os.getenv("windows_7_64_bit")) if os.getenv("windows_7_64_bit") is not None else 0
-windows_7_32_bit = int(os.getenv("windows_7_32_bit")) if os.getenv("windows_7_32_bit") is not None else 0
-windows_server_2016 = int(os.getenv("windows_server_2016")) if os.getenv("windows_server_2016") is not None else 0
-windows_server_2019 = int(os.getenv("windows_server_2019")) if os.getenv("windows_server_2019") is not None else 0
-centOS_6 = int(os.getenv("centOS_6")) if os.getenv("centOS_6") is not None else 0
-centOS_7 = int(os.getenv("centOS_7")) if os.getenv("centOS_7") is not None else 0
-centOS_8 = int(os.getenv("centOS_8")) if os.getenv("centOS_8") is not None else 0
-ubuntu_16 = int(os.getenv("ubuntu_16")) if os.getenv("ubuntu_16") is not None else 0
-ubuntu_18 = int(os.getenv("ubuntu_18")) if os.getenv("ubuntu_18") is not None else 0
-ubuntu_20 = int(os.getenv("ubuntu_20")) if os.getenv("ubuntu_20") is not None else 0
+# External Service Deployment data
+aggregators_amount = int(os.getenv("aggregators_amount", default=1))
+cores_amount = int(os.getenv("cores_amount", default=1))
+
+windows_11_64_bit = int(os.getenv("windows_11_64_bit", default=0))
+windows_10_64_bit = int(os.getenv("windows_10_64_bit", default=0))
+windows_10_32_bit = int(os.getenv("windows_10_32_bit", default=0))
+windows_8_64_bit = int(os.getenv("windows_8_64_bit", default=0))
+windows_8_32_bit = int(os.getenv("windows_8_32_bit", default=0))
+windows_7_64_bit = int(os.getenv("windows_7_64_bit", default=0))
+windows_7_32_bit = int(os.getenv("windows_7_32_bit", default=0))
+windows_server_2016 = int(os.getenv("windows_server_2016", default=0))
+windows_server_2019 = int(os.getenv("windows_server_2019", default=0))
+centOS_6 = int(os.getenv("centOS_6", default=0))
+centOS_7 = int(os.getenv("centOS_7", default=0))
+centOS_8 = int(os.getenv("centOS_8", default=0))
+ubuntu_16 = int(os.getenv("ubuntu_16", default=0))
+ubuntu_18 = int(os.getenv("ubuntu_18", default=0))
+ubuntu_20 = int(os.getenv("ubuntu_20", default=0))
--- infra/forti_edr_versions_service_handler/forti_edr_versions_service_handler.py
+++ infra/forti_edr_versions_service_handler/forti_edr_versions_service_handler.py
@@ -14,13 +14,16 @@ class FortiEdrVersionsServiceHandler:
         This method returns the latest builds of various system components according to base version.
         it sends request to service which provide this information on a "automation services machine"
         :param base_version: for example 5.2.0
-        :param num_build: number of required latest builds, 1 is the default and will return the latest build
-        :return: dictionary with latest build of the various system components.
+        :param num_builds: number of required the latest builds, 1 is the default and will return the latest build
+        :return: dictionary with the latest build of the various system components.
         """
         if base_version.count('.') != 2:
             assert False, "Incorrect template of base version, insert something such as 5.1.0 (x.y.z)"
 
-        url = f'http://{third_party_details.AUTOMATION_SERVICES_UTILS_MACHINE_IP}:{third_party_details.LATEST_VERSIONS_SERVICE_PORT}/latest_build?base_version={base_version}&num_builds={num_builds}'
+        url = (
+            f'{third_party_details.AUTOMATION_SERVICES_URL}/'
+            f'latest_build?base_version={base_version}&num_builds={num_builds}'
+        )
         versions_dict = HttpRequesterUtils.send_request(request_method=HttpRequestMethodsEnum.GET,
                                                         url=url,
                                                         expected_status_code=200)
@@ -29,10 +32,11 @@ class FortiEdrVersionsServiceHandler:
     @staticmethod
     @allure.step("Extract last {num_last_content_files} content files from shared folder")
     def get_latest_content_files_from_shared_folder(num_last_content_files: int = 1):
-        url = f'http://{third_party_details.AUTOMATION_SERVICES_UTILS_MACHINE_IP}:{third_party_details.LATEST_VERSIONS_SERVICE_PORT}/list_content?num_last_content_files={num_last_content_files}'
+        url = (
+            f'{third_party_details.AUTOMATION_SERVICES_URL}'
+            f'/list_content?num_last_content_files={num_last_content_files}'
+        )
         versions_dict = HttpRequesterUtils.send_request(request_method=HttpRequestMethodsEnum.GET,
                                                         url=url,
                                                         expected_status_code=200)
         return versions_dict
-
-
